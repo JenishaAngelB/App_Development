69bcfab85222d1ee1015c8a50a9c22994af5de7f
JenishaAngelB <145896063+JenishaAngelB@users.noreply.github.com>
2024-08-14 10:48:14 +0530

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
--- backend/src/main/java/com/example/backend/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-}
